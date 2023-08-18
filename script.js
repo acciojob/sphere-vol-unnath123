1c8094ebadea9a1104ba4439c641278c30e4fcb5
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-19 00:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	let rad=document.getElementById("radius").value;
+	let volume1=10*rad;
+	
+	document.getElementById("volume").value=volume1;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
